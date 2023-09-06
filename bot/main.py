955faab0de1aa90f0a77d347a4ff8218c6ff5cd4
ty0018 <yingtong0333@gmail.com>
2023-09-06 22:27:32 +0800

    finished /start /onboard /info commands

--- bot/main.py
+++ bot/main.py
@@ -1,74 +1,56 @@
 import logging
 import time
 from credentials import BOT_TOKEN, URL
-from messages import START, ONBOARD, INFO_LINK, ONBOARD_REPLY, ERROR
+from messages import START, ONBOARD_START, WIKI, ONBOARD_REPLY, ONBOARD_TEST, ERROR, ONBOARD_DONE, PRIVACY_POLICY, QUIZ
 
 import flask
 
 from telebot import TeleBot, types, logger
+from telebot.util import quick_markup
 
 API_TOKEN = BOT_TOKEN
 
-WEBHOOK_HOST = URL
-WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
-WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+# WEBHOOK_HOST = URL
+# WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+# WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
-WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
-WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
+# WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+# WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
 
-# Quick'n'dirty SSL certificate generation:
-#
-# openssl genrsa -out webhook_pkey.pem 2048
-# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
-#
-# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
-# with the same value in you put in WEBHOOK_HOST
+# # Quick'n'dirty SSL certificate generation:
+# #
+# # openssl genrsa -out webhook_pkey.pem 2048
+# # openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
+# #
+# # When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
+# # with the same value in you put in WEBHOOK_HOST
 
-WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
+# WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+# WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
 
 logger = logger
 logger.setLevel(logging.INFO)
 
 bot = TeleBot(API_TOKEN)
 
-app = flask.Flask(__name__)
+# app = flask.Flask(__name__)
 
-def gen_markup(row_width):
-    markup = types.InlineKeyboardMarkup()
-    markup.row_width = row_width
-    markup.add(types.InlineKeyboardButton("Yes", callback_data="cb_yes"),
-                               types.InlineKeyboardButton("No", callback_data="cb_no"))
-    return markup
-
-@bot.callback_query_handler(func=lambda call: True)
-def callback_query(call):
-    if call.data == "cb_yes":
-        bot.answer_callback_query(call.id, "Answer is Yes")
-    elif call.data == "cb_no":
-        bot.answer_callback_query(call.id, "Answer is No")
-
-@bot.message_handler(func=lambda message: True)
-def message_handler(message):
-    bot.send_message(message.chat.id, "Yes/no?", reply_markup=gen_markup())
+# # Empty webserver index, return nothing, just http 200
+# @app.route('/', methods=['GET', 'HEAD'])
+# def index():
+#     return ''
 
 
-# Empty webserver index, return nothing, just http 200
-@app.route('/', methods=['GET', 'HEAD'])
-def index():
-    return ''
-
-
-# Process webhook calls
-@app.route(WEBHOOK_URL_PATH, methods=['POST'])
-def webhook():
-    if flask.request.headers.get('content-type') == 'application/json':
-        json_string = flask.request.get_data().decode('utf-8')
-        update = types.Update.de_json(json_string)
-        bot.process_new_updates([update])
-        return ''
-    else:
-        flask.abort(403)
+# # Process webhook calls
+# @app.route(WEBHOOK_URL_PATH, methods=['POST'])
+# def webhook():
+#     if flask.request.headers.get('content-type') == 'application/json':
+#         json_string = flask.request.get_data().decode('utf-8')
+#         update = types.Update.de_json(json_string)
+#         bot.process_new_updates([update])
+#         return ''
+#     else:
+#         flask.abort(403)
 
 
 # Handle '/start' - to add logic for activating message receiving
@@ -81,41 +63,67 @@ def send_start(message):
 @bot.message_handler(commands=['onboard'])
 def send_onboard(message):
     chat_id = message.chat.id
-    #users must reply to this message
-    markup = types.ForcedReply(selective=False)
-    bot.send_message(chat_id,ONBOARD, reply_markup=markup)
-    
+    #users must reply their name to this message
+    markup = types.ForceReply(selective=True)
+    bot.send_message(chat_id,ONBOARD_START, reply_markup=markup)
 
 #handle '/info'
 @bot.message_handler(commands=['info'])
 def send_info(message):
     chat_id = message.chat.id
-    bot.send_message(chat_id,INFO_LINK)
+    markup = quick_markup({
+            'Privacy Policy': {'url': PRIVACY_POLICY},
+            'Wiki Page': {'url': WIKI}}, 
+            row_width=1)
+    bot.send_message(chat_id,"Check out below for our resources!", reply_markup=markup)
     
 
 # Handle all other messages
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def reply_message(message):
-    if message.reply_to_message.text == ONBOARD:
+    #if user replies name
+    if message.reply_to_message.text == ONBOARD_START:
         chat_id = message.chat.id
-        markup = types.InlineKeyboardMarkup(types.InlineKeyboardButton("Got it!", callback_data="done"))
-        bot.send_message(chat_id, ONBOARD_REPLY.format(message.text), reply_markup = markup)
-
+        markup = quick_markup({
+            'Privacy Policy': {'url': PRIVACY_POLICY},
+            'Got it!': {'callback_data': 'Understood privacy terms'}}, 
+            row_width=1)
+        bot.send_message(chat_id, ONBOARD_REPLY.format(name = message.text), reply_markup = markup)
+    #if user replies plain text
     else:
         bot.reply_to(message, ERROR)
 
+#handles callback queries from inline keyboard buttons
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == "Understood privacy terms":
+        markup = quick_markup({"Take me to quiz!":{"url": QUIZ},
+                               "I've done the quiz!": {"callback_data":"Quiz done"}}, 
+                               row_width=1)
+        # bot.answer_callback_query(call.id, ONBOARD_TEST, reply_markup = markup)
+        # bot.edit_message_text(ONBOARD_TEST, call.message.chat.id, call.message.id,reply_markup=markup)
+        bot.send_message(call.from_user.id, ONBOARD_TEST, reply_markup=markup)
+    elif call.data == "Quiz done":
+        bot.send_message(call.from_user.id, ONBOARD_DONE)
 
-# Remove webhook, it fails sometimes the set if there is a previous webhook
-bot.remove_webhook()
+#deactivate command
 
-time.sleep(0.1)
+bot.infinity_polling()
 
-# Set webhook
-bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
-                certificate=open(WEBHOOK_SSL_CERT, 'r'))
 
-# Start flask server
-app.run(host=WEBHOOK_LISTEN,
-        port=WEBHOOK_PORT,
-        ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
-        debug=True)
+#webhook set up below
+
+# # Remove webhook, it fails sometimes the set if there is a previous webhook
+# bot.remove_webhook()
+
+# time.sleep(0.1)
+
+# # Set webhook
+# bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+#                 certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+# # Start flask server
+# app.run(host=WEBHOOK_LISTEN,
+#         port=WEBHOOK_PORT,
+#         ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
+#         debug=True)
---
+++ bot/messages.py
@@ -0,0 +1,12 @@
+START = "Hello, you have activated the message portal! You will begin to receive messages for checking soon!"
+
+ONBOARD_START="Welcome to our community of CheckMates! We're grateful to have you on board to combat misinformation and scams. We'd love to get to know you better - could you **share your name** with us by replying to this message?"
+ONBOARD_REPLY = "Thank you and welcome, {name}! We're thrilled to have you on board as we work together to combat misinformation and scams. :)\n\nBy using the CheckMate bot, you are accepting our privacy policy (Click button below to find out more!)"
+ONBOARD_TEST = "To ensure you're equipped with the necessary skills to identify misinformation and scams, let's start with a quick quiz (Click on 'Take me to quiz!'). Once you've completed it, come back to this chat and click on 'I've done the quiz!' to notify me.\n\nLet's get started!"
+ONBOARD_DONE = "Awesome! Now that you know how to identify misinformation and scams, you are ready to help us combat them!\n\nUse the '/info' command for more resources and use the '/start' command to activate receiving of messages.\n\nThanks again for joining our community of CheckMates!"
+
+ERROR="Sorry, I do not understand what you are saying. Try '/' to see available commands instead!"
+
+WIKI= "https://bit.ly/checkmates-wiki"
+PRIVACY_POLICY = "https://bit.ly/checkmate-privacy"
+QUIZ = "https://bit.ly/checkmates-quiz"
